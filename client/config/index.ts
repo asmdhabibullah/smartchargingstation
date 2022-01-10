57145f89652a159840c1765f5a4976bd3f13a690
asmdhabibullah <asmdhabibullah@yahoo.com>
2022-01-11 01:21:35 +0800

    deleted

--- client/config/index.ts
+++
@@ -1,9 +0,0 @@
-// import getConfig from "next/config";
-// const { publicRuntimeConfig } = getConfig();
-
-// Private
-export const config = {
-    ENDPOINT: process.env.ENDPOINT
-};
-
-// console.log(config.ENDPOINT);
