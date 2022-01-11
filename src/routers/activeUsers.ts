3132c73d0d4c82604c184e05d27c634ab22cc3f7
asmdhabibullah <asmdhabibullah@yahoo.com>
2022-01-12 06:04:45 +0800

    updated routers

--- src/routers/activeUsers.ts
+++ src/routers/activeUsers.ts
@@ -1,17 +1,17 @@
 import express, { Request, Response } from "express";
 import { successHandler, errorHandler } from "package.breezebd.com";
 import { User } from "../models/user";
-import { rdsClint, cacheCheck } from './middleware';
+// import { rdsClint, cacheCheck } from './middleware';
 
 const router = express.Router();
 
-router.get("/api/users/active", cacheCheck, async (req: Request, res: Response) => {
+router.get("/api/users/active",  async (req: Request, res: Response) => {
     // console.log("HI, I'm active user.");
     const { key } = req.body;
     await User.find({ status: true }).exec((err, docs) => {
         // console.log(err, docs);
         if (!err && docs.length > 0 && docs.length > 0) {
-            rdsClint.setex(key, 86400, docs as any);
+            // rdsClint.setex(key, 86400, docs as any);
             return successHandler(res, 200, docs);
         }
         return errorHandler(res, 404, "Active user not found!")
